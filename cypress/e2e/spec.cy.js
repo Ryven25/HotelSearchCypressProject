62be7d67ba5fd516a5ed062943c41cd45cf09be7
Roman Fishman <ryven25@gmail.com>
2023-03-09 15:54:59 -0500

    first commit

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,9 @@
+///<reference types = "Cypress"/>
+
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://example.cypress.io')
+    cy.url().should('eq', 'https://example.cypress.io/')
+  })
+})
+
